d769f5d7edc690c39e4ecf0847b15929b9b1e3de
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-07-21 20:24:10 +0300

    добавил пустое поле 2048 + стили

--- src/pages/Game2048.jsx
+++ src/pages/Game2048.jsx
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
+
 export default function Game2048() {
+
+  const [grid, setGrid] = useState(createEmptyGrid());
+
+  function createEmptyGrid() {
+    return Array(4).fill().map(() => Array(4).fill(0));
+  }
     return  <div>
-      <h1>Добро пожаловать в игру 2048</h1>
-      <p>Здесь будeт 2048</p>
-      
-      </div>
+              <h1>2048</h1>
+              <div className="game2048-container">
+              <div className="grid2048">
+                {grid.map((row, rowIndex) =>
+                  row.map((cell, colIndex) => (
+                    <div key={`${rowIndex}-${colIndex}`} className="tile">
+                      {cell}
+                    </div>
+                  ))
+                )}
+              </div>
+            </div>
+           </div>
 }
